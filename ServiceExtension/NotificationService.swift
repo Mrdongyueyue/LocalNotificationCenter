23d321bb28b022daf88268bb4da7439c3b32ba12
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-04-06 17:32:49 +0800

    fix

--- ServiceExtension/NotificationService.swift
+++ ServiceExtension/NotificationService.swift
@@ -59,7 +59,7 @@ class NotificationService: UNNotificationServiceExtension, URLSessionDelegate {
             } catch  {
                 
             }
-            
+            return nil
         }
         
         
